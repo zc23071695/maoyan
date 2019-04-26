54b63ce3bb580181134300a16cacbee45e39e1fb
sakitomosimasu <354418985@qq.com>
2019-04-26 15:20:28 +0800

    the end

--- src/router/router.js
+++ src/router/router.js
@@ -1,41 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Index from '../components/index/Index.vue'
-import Movie from '../components/index/Movie.vue'
-import Cinema from '../components/index/Cinema.vue'
-import User from '../components/index/User.vue'
-import City from '../components/city/City.vue'
-import Serch from '../components/serch/Serch.vue'
-import Detail from '../components/detail/Detail.vue'
+// import Index from '../components/index/Index.vue'
+// import Movie from '../components/index/Movie.vue'
+// import Cinema from '../components/index/Cinema.vue'
+// import User from '../components/index/User.vue'
+// // import City from '../components/city/City.vue'
+// import Serch from '../components/serch/Serch.vue'
+// import Detail from '../components/detail/Detail.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    component: Index,
+    component: () => import('../components/index/Index.vue'),
     children: [
       {
         path: 'movie',
-        component: Movie,
+        component: () => import('../components/index/Movie.vue'),
         name: 'movie',
         meta: { tabNumber: 0 }
       },
       {
         path: 'cinema',
-        component: Cinema,
+        component: () => import('../components/index/Cinema.vue'),
         name: 'cinema',
         meta: { tabNumber: 1 }
       },
-      { path: 'user', component: User, name: 'user', meta: { tabNumber: 2 } },
+      { path: 'user', component: () => import('../components/index/User.vue'), name: 'user', meta: { tabNumber: 2 } },
       { path: '', redirect: '/movie' }
     ]
   },
-  { path: '/city', component: City, name: 'city' },
-  { path: 'serch', component: Serch, name: 'serch' },
-  { path: '/movie/:id', component: Detail, name: 'detail' },
-  { path: 'city', component: City, name: 'city' },
-  { path: 'serch', component: Serch, name: 'serch' },
+  // { path: '/city', component: City, name: 'city' },
+  { path: 'serch', component: () => import('../components/serch/Serch.vue'), name: 'serch' },
+  { path: '/movie/:id', component: () => import('../components/detail/Detail.vue'), name: 'detail' },
   { path: '*', redirect: '/movie' }
 ]
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  publicPath: '/my/',
+
   devServer: {
     proxy: {
       '/maoyan': {
